04787db30ce463379ac506748f075977a1c35937
hatter <hatter@hatter-ubuntu.(none)>
2013-11-04 20:21:51 +0300

    before add admin attribute

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
 
 private
 
@@ -9,4 +11,13 @@ private
     @site = Site.where('domain = ?', request.host).first!
   end
   helper_method :current_site
+
+protected
+
+  def configure_permitted_parameters
+   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :login, :email, :password, :password_confirmation) }
+   devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :login, :email, :password, :password_confirmation) }
+   devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :login, :email, :password, :password_confirmation, :current_password) }
+  end
+
 end
--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -88,13 +88,6 @@ class SitesController < ApplicationController
     end
 
 
-    def correct_user
-      @corect_site = current_user.sites.find(params[:id])
-      rescue ActiveRecord::RecordNotFound
-        redirect_to root_url
-        flash[:error] = "current_user is not fk admin!"
-    end
-
 
 
     # Never trust parameters from the scary internet, only allow the white list through.
